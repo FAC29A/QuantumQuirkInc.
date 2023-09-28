e00e772061cb52139456f18746802eae2df47b22
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2023-09-28 20:09:49 +0100

    Delete html team and create team object in JS

---
+++ script.js
@@ -0,0 +1,47 @@
+//Here we store each team member's information in an object
+
+const teamMembers = [
+    {
+        name: "Dr. Quark E. Scientist",
+        position: "CEO",
+        img: '',
+        bio: "The brilliant, yet eccentric, founder of the agency who claims to have a PhD in Quantum Quirkology.",
+        id: 1,
+    },
+    {
+        name: "Mystique Maven",
+        position: "Esoteric Consultant",
+        img: '',
+        bio: "A mysterious and enigmatic fortune teller who handles client consultations, using tarot cards and crystal balls to predict future trends.",
+        id: 2,
+    },
+    {
+        name: "Captain Quasar",
+        position: "Astronaut",
+        img: '',
+        bio: "The agency's resident astronaut and space explorer who assures clients that their marketing campaigns will truly be out of this world.",
+        id: 3,
+    },
+    {
+        name: "Crypto-Kate",
+        position: "Cryptozoologist",
+        img: '',
+        bio: "The cryptozoologist-in-chief who swears by her ability to optimize websites for elusive creatures.",
+        id: 4,
+    },
+    {
+        name: "Telepathic Tim",
+        position: "Creative Medium",
+        img: '',
+        bio: "The graphic designer who claims to know what you want even before you do, thanks to his telepathic design skills.",
+        id: 5,
+    },
+    {
+        name: "Zog the Alien",
+        position: "Alien",
+        img: '',
+        bio: "The agency's extraterrestrial consultant who communicates in a language only decipherable by the agency's clients.",
+        id: 6,
+    },
+];
+
